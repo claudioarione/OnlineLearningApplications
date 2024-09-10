63af78a31ded61d94c2bd4099c4b7b32158d092b
Void <100213120+iVoid73@users.noreply.github.com>
2024-09-10 19:44:39 +0200

    parametrize some hard coded numbers

--- agents/ucb_like/ucb1.py
+++ agents/ucb_like/ucb1.py
@@ -11,36 +11,28 @@ class UCB1Agent:
 
         self.a_t = None
         self.average_rewards = np.zeros(self.K)
-        self.average_costs = np.zeros(self.K)
         self.N_pulls = np.zeros(self.K)
         self.t = 0
 
         self.budget = budget
         self.valuation = valuation
-        self.rho = self.budget/self.T
 
     def bid(self):
       if self.budget < 1:
             return 0
       else:
         if self.t < self.K:
-            self.a_t = (self.t) / (self.K-1) #rescaled bid
+            self.a_t = self.t
         else:
             ucbs_rewards = self.average_rewards + self.range*np.sqrt(2*np.log(self.t)/self.N_pulls) #can also use self.T instead of self.t
-            ucbs_costs = self.average_costs - self.range*np.sqrt(2*np.log(self.t)/self.N_pulls) #can also use self.T instead of self.t
-
-            weights = compute_weights(self.rho, ucbs_rewards, ucbs_costs)
-            self.a_t = np.random.choice(np.arange(self.K), p=weights) / (self.K-1)
-
-            #self.a_t = np.argmax(ucbs_rewards) / self.K
-        return self.a_t
+            self.a_t = np.argmax(ucbs_rewards)
+        return self.bids_set[self.a_t]
 
     def update(self, r_t, c_t):
-        index = int(self.a_t*(self.K-1))
+        index = self.a_t
 
         self.N_pulls[index] += 1
         self.average_rewards[index] += (r_t - self.average_rewards[index])/self.N_pulls[index]
-        self.average_costs[index] += (c_t - self.average_costs[index])/self.N_pulls[index]
 
         self.t += 1
         self.budget -= c_t
